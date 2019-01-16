c6a939744f248dd517e9a32a1cfd555d991470aa
TealSquirrel <tealwizardtapestries@gmail.com>
2019-01-15 21:00:09 -0600

    Added Maven stuff that randomly disappeared on me. Ugh.

---
+++ src/main/java/net/tealsquirrel/mute/MuteApplication.java
@@ -0,0 +1,14 @@
+package net.tealsquirrel.mute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuteApplication.class, args);
+	}
+
+}
+
